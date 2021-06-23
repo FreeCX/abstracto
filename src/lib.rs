8dbd7df3ecb38f0e6af0c7f3930be3583c1ec7c4
Alexey Golubev <dr.freecx@gmail.com>
2021-06-23 23:53:54 +0300

    small code optimization

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufWriter, Write};
@@ -70,49 +69,53 @@ impl Generator {
     }
 
     pub fn fill(&self, func: &RenderFunc) -> RenderResult {
+        let iterations = self.width * self.height;
         let mut result = RenderResult {
-            raw: Vec::new(),
+            raw: vec![0.0; iterations as usize],
             width: self.width,
             height: self.height,
         };
+        let mut minv: Option<f32> = None;
+        let mut maxv: Option<f32> = None;
 
         let x_step = (self.x_right - self.x_left) / self.width as f32;
         let y_step = (self.y_right - self.y_left) / self.height as f32;
-        let iterations = self.width * self.height;
 
-        result.raw = (0..iterations)
-            .map(|index| {
-                let ix = index % self.width;
-                let iy = index / self.width;
-                func(RenderPoint {
-                    xv: self.x_left as f32 + ix as f32 * x_step,
-                    yv: self.y_left as f32 + iy as f32 * y_step,
-                    xc: ix,
-                    yc: iy,
-                })
-            })
-            .collect();
+        // inplace calculating
+        for (index, value) in result.raw.iter_mut().enumerate() {
+            let ix = index as u32 % self.width;
+            let iy = index as u32 / self.width;
+            *value = func(RenderPoint {
+                xv: self.x_left as f32 + ix as f32 * x_step,
+                yv: self.y_left as f32 + iy as f32 * y_step,
+                xc: ix,
+                yc: iy,
+            });
+
+            // find min & max
+            minv = match minv {
+                Some(v) => Some(value.min(v)),
+                None => Some(*value)
+            };
+            maxv = match maxv {
+                Some(v) => Some(value.max(v)),
+                None => Some(*value)
+            };
+        }
 
         // no data - no remap
         if result.raw.len() == 0 {
             return result;
         }
 
-        // remap values to [0, 1]
-        let minv = result
-            .raw
-            .iter()
-            .min_by(|&x, &y| x.partial_cmp(y).unwrap_or(Ordering::Equal))
-            .unwrap()
-            .abs();
-        let maxv = result
-            .raw
-            .iter()
-            .max_by(|&x, &y| x.partial_cmp(y).unwrap_or(Ordering::Equal))
-            .unwrap()
-            .abs();
+        let minv = minv.unwrap().abs();
+        let maxv = maxv.unwrap().abs();
         let range = minv + maxv;
-        result.raw = result.raw.iter().map(|x| (x + minv) / range).collect();
+
+        // remap values to [0, 1]
+        for value in result.raw.iter_mut() {
+            *value = (*value + minv) / range;
+        }
 
         result
     }
@@ -129,16 +132,21 @@ impl Color {
 }
 
 pub fn generate_ppm(output: &str, render: &RenderResult, palette: &Palette) {
-    let mut f = BufWriter::new(File::create(output).unwrap());
+    // i don't know exact header size
+    let header_hint_size = 20;
+    // allocate image buffer
+    let mut buffer = Vec::with_capacity(3 * render.width as usize * render.height as usize + header_hint_size);
     let default = Color::rgb(0, 0, 0);
 
-    // header
-    let _ = write!(f, "P6\n{} {}\n255\n", render.width, render.height);
-
+    // fill buffer
+    let _ = write!(buffer, "P6\n{} {}\n255\n", render.width, render.height);
     for point in &render.raw {
-        // remap to palette
         let index = ((palette.len() - 1) as f32 * (*point)) as ColorIndex;
         let color = palette.get(&index).unwrap_or(&default);
-        let _ = f.write(&[color.r, color.g, color.b]);
+        buffer.extend_from_slice(&[color.r, color.g, color.b]);
     }
+
+    // write to file
+    let mut f = BufWriter::new(File::create(output).unwrap());
+    let _ = f.write(&buffer);
 }
